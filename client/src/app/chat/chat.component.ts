482c4efe90c81c02d3e08ba00c58ab25bbb09fbe
sverevkin25@gmail.com <82106610Vereva>
2019-06-20 18:01:30 +0300

    chat real socket

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -1,26 +1,18 @@
-import {
-  AfterContentChecked,
-  AfterContentInit,
-  AfterViewChecked,
-  AfterViewInit,
-  Component,
-  DoCheck,
-  OnChanges,
-  OnDestroy,
-  OnInit
-} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {SocketService} from '../shared/services/socket.service';
 import {MessageModel} from '../models/message.model';
 import {Observable, Subscription} from 'rxjs';
 import {UserModel} from '../models/user.model';
-import {typeIsOrHasBaseType} from 'tslint/lib/language/typeUtils';
+import {debounceTime, take, takeUntil, tap} from 'rxjs/operators';
+import {fromEvent} from "rxjs/internal/observable/fromEvent";
+import {Subject} from "rxjs/internal/Subject";
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit, OnDestroy, AfterViewInit {
+export class ChatComponent implements OnInit, AfterViewInit, OnDestroy {
 
   msgVal = '';
   messages: Observable<MessageModel[]>;
@@ -31,13 +23,17 @@ export class ChatComponent implements OnInit, OnDestroy, AfterViewInit {
 
   sub: Subscription;
 
+  destroy: Subject<any> = new Subject<any>();
+
+  @ViewChild('inputMessage') input: ElementRef;
+
   constructor(
     private socketService: SocketService,
   ) {
   }
 
   ngOnInit() {
-    this.typingMessage(false);
+    // this.typingMessage(false);
     this.messages = this.socketService.getMessages();
     this.socketService.addOrGetUser('getLoggedUsers')
       .subscribe((data: any[]) => {
@@ -47,6 +43,7 @@ export class ChatComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngAfterViewInit() {
+    this.subscribeTotyping();
   }
 
   chatSend(theirMessage: string) {
@@ -86,20 +83,55 @@ export class ChatComponent implements OnInit, OnDestroy, AfterViewInit {
       return 'gainsboro';
     }
   }
-
-  typingMessage(status) {
-    const userName = JSON.parse(localStorage.getItem('token'));
-
-    this.sub = this.socketService.typing(status, userName.name)
-      .subscribe((data) => {
-        this.typing = data;
-      });
-    setTimeout(() => {
-      this.typing = false;
-    }, 5000);
-  }
+  //
+  // typingMessage(status) {
+  //
+  //   const userName = JSON.parse(localStorage.getItem('token'));
+  //
+  //   this.sub = this.socketService.typing(status, userName.name)
+  //     .pipe(
+  //       take(1),
+  //       takeUntil(this.destroy),
+  //     )
+  //     .subscribe((data) => {
+  //       this.typing = data;
+  //       console.log(this.typing.status);
+  //
+  //       if (this.typing.status) {
+  //         setTimeout(() => {
+  //           this.typing.status = false;
+  //           console.log(this.typing.status);
+  //         }, 5000);
+  //       }
+  //     });
+  // }
 
   ngOnDestroy() {
-    this.sub.unsubscribe();
+    this.destroy.next();
+    this.destroy.unsubscribe();
   }
+
+  subscribeTotyping() {
+    let startTyping = false;
+
+    fromEvent(this.input.nativeElement, 'keydown').pipe(
+      takeUntil(this.destroy),
+      tap(() => {
+        if (!startTyping) {
+          console.log('startTyping');
+          startTyping = true;
+        }
+      }),
+      debounceTime(5000),
+      tap(() => {
+        if (startTyping) {
+          console.log('endtyping');
+          startTyping = false;
+        }
+      }),
+    ).subscribe();
+
+  }
+
+
 }
